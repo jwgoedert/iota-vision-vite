510e371f91f0bc09ae778aeec881959ca5736ee2
James Wesley Goedert <jwgoedert@gmail.com>
2023-11-18 16:01:41 -0600

    Make contact a child of root so it renders in the layout section

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,12 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
   {
     path: "contacts/:contactId",
